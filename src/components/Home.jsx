8c8085d77308013bc15a1db4e521ad38f7b73157
Geovanna Otoni <geovannaotoni@gmail.com>
2023-04-19 17:26:29 -0300

    requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { IconButton } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    categories: [], // salva o array de categorias retornado pela API no state
+  };
+
+  async componentDidMount() {
+    const categoriesFromAPI = await getCategories();
+    this.setState({
+      categories: categoriesFromAPI,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <>
         <input type="text" />
@@ -16,7 +29,29 @@ class Home extends Component {
         <div data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </div>
-
+        <nav>
+          <p>Categorias</p>
+          <ul>
+            {
+              categories.map((category) => (
+                <li key={ category.id }>
+                  <label
+                    htmlFor={ category.id }
+                    data-testid="category"
+                  >
+                    <input
+                      type="radio"
+                      name="category"
+                      id={ category.id }
+                      value={ category.id }
+                    />
+                    { category.name }
+                  </label>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
       </>
     );
   }
