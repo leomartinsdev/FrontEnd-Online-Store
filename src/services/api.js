994bb26f18568d9abb0662d402a5a83bb8b9c424
Geovanna Otoni <geovannaotoni@gmail.com>
2023-04-19 15:51:57 -0300

    função getCategories feita

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
