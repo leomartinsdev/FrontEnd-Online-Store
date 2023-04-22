a3bcd567f90d8294a8eb26f57f528d28627ba792
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2023-04-22 13:09:41 -0300

    Primeira parte do 11 feito. Me enrolando um pouco para a segunda parte

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -52,6 +52,75 @@ export default class ProductDetail extends Component {
         <Link data-testid="shopping-cart-button" to="/cart">
           <button type="button">Carrinho</button>
         </Link>
+
+        <h2>Avaliação do produto</h2>
+
+        <form>
+          <label>
+            <input
+              type="email"
+              placeholder="Email"
+              data-testid="product-detail-email"
+            />
+          </label>
+          <label>
+            1
+            <input
+              type="radio"
+              data-testid="1-rating"
+              value="1"
+              name="radioButton"
+            />
+          </label>
+          <label>
+            2
+            <input
+              type="radio"
+              data-testid="2-rating"
+              value="2"
+              name="radioButton"
+            />
+          </label>
+          <label>
+            3
+            <input
+              type="radio"
+              data-testid="3-rating"
+              value="3"
+              name="radioButton"
+            />
+          </label>
+          <label>
+            4
+            <input
+              type="radio"
+              data-testid="4-rating"
+              value="4"
+              name="radioButton"
+            />
+          </label>
+          <label>
+            5
+            <input
+              type="radio"
+              data-testid="5-rating"
+              value="5"
+              name="radioButton"
+            />
+          </label>
+          <label>
+            <input
+              type="text"
+              data-testid="product-detail-evaluation"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="submit-review-btn"
+          >
+            Enviar
+          </button>
+        </form>
       </div>
     );
   }
